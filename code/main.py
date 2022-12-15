efdcc9009dedbd03f06630910e35ca5f5d4a02a9
Casper Burik <cburik@users.noreply.github.com>
2022-12-15 14:03:41 +0100

    Adds dataprocessing
    Changes:
        - Adds filtering and merging of the two dataframes
        - Adds optional countries arg to argparser

--- code/main.py
+++ code/main.py
@@ -1,5 +1,6 @@
 import argparse
 from pyspark.sql import SparkSession, DataFrame
+from pyspark.sql.functions import col
 from typing import Tuple
 
 
@@ -21,18 +22,42 @@ def read_data(spark: SparkSession,
     return clientDF, financialDF
 
 
-def main(client_csv, financial_csv):
+def process_data(clientDF: DataFrame,
+                 financialDF: DataFrame,
+                 countries: list) -> DataFrame:
+    """
+    Processes DataFrames, including filtering and merging
+    """
+    if countries:
+        clientDF = clientDF.filter(
+                clientDF.country.isin(countries)
+            ).select(
+                clientDF.id, clientDF.email
+            )
+    else:
+        clientDF = clientDF.select(clientDF.id, clientDF.email)
+
+    financialDF = financialDF.select(
+            financialDF.id, financialDF.btc_a, financialDF.cc_t
+        )
+
+    df = clientDF.join(financialDF, ['id'])
+    return df
+
+
+def main(client_csv: str, financial_csv: str, countries: list):
     """
     main
     """
     spark = create_session()
     clientDF, financialDF = read_data(spark, client_csv, financial_csv)
-
+    df = process_data(clientDF, financialDF, countries)
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
     parser.add_argument('client_csv')
     parser.add_argument('financial_csv')
+    parser.add_argument('--countries', nargs='*', dest='countries', default=[])
     args = parser.parse_args()
-    
-    main(args.client_csv, args.financial_csv)
+
+    main(args.client_csv, args.financial_csv, args.countries)
