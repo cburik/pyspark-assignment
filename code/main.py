e8a37718538967b36362b99b5cdc6947cb69360b
Casper Burik <cburik@users.noreply.github.com>
2022-12-15 15:01:32 +0100

    Adds optional commandline argument for output directory

--- code/main.py
+++ code/main.py
@@ -49,11 +49,11 @@ def process_data(clientDF: DataFrame,
     return df
 
 
-def output(df: DataFrame, folder: str):
+def output(df: DataFrame, outdir: str):
     """
     Outputs data to csv
     """
-    df.write.format('csv').mode('overwrite').options(header=True).save(folder)
+    df.write.format('csv').mode('overwrite').options(header=True).save(outdir)
 
 
 def main(client_csv: str,
@@ -64,14 +64,14 @@ def main(client_csv: str,
             'btc_a': 'bitcoin_address',
             'cc_t': 'credit_card_type'
             },
-         folder: str = 'client_data/'):
+         outdir: str = 'client_data/'):
     """
     main
     """
     spark = create_session()
     clientDF, financialDF = read_data(spark, client_csv, financial_csv)
     df = process_data(clientDF, financialDF, countries, rename)
-    output(df, folder=folder)
+    output(df, outdir=outdir)
 
 
 if __name__ == '__main__':
@@ -79,6 +79,7 @@ if __name__ == '__main__':
     parser.add_argument('client_csv')
     parser.add_argument('financial_csv')
     parser.add_argument('--countries', nargs='*', dest='countries', default=[])
+    parser.add_argument('--outdir', dest='outdir', default='client_data/')
     args = parser.parse_args()
 
-    main(args.client_csv, args.financial_csv, args.countries)
+    main(args.client_csv, args.financial_csv, args.countries, outdir=args.outdir)
