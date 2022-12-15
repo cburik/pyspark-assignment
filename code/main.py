683d27f00900728024ded0b743dc1278b67c2fc4
Casper Burik <cburik@users.noreply.github.com>
2022-12-15 16:13:37 +0100

    Adds logging

--- code/main.py
+++ code/main.py
@@ -1,4 +1,5 @@
 import argparse
+import logging
 from pyspark.sql import SparkSession, DataFrame
 from pyspark.sql.functions import col
 from typing import Tuple
@@ -8,6 +9,7 @@ def create_session() -> SparkSession:
     """
     creates SparkSession
     """
+    logging.debug('Starting SparkSession')
     return SparkSession.builder.getOrCreate()
 
 
@@ -17,8 +19,18 @@ def read_data(spark: SparkSession,
     """
     reads data from csv files into Spark DataFrames
     """
+    logging.info('Reading data from files: '
+                 'client data: "{}"; '
+                 'financial data: "{}"'.format(client_csv, financial_csv))
+
     clientDF = spark.read.csv(client_csv, header=True)
     financialDF = spark.read.csv(financial_csv, header=True)
+    logging.info(
+        "Number of lines in client data: {}".format(clientDF.count())
+        )
+    logging.info(
+        "Number of lines in financial data: {}".format(financialDF.count())
+        )
     return clientDF, financialDF
 
 
@@ -29,13 +41,16 @@ def process_data(clientDF: DataFrame,
     """
     Processes DataFrames, including filtering, merging and renaming
     """
+    logging.info('Filtering data...')
     if countries:
+        logging.info('Filtering on Countries: {}'.format(countries))
         clientDF = clientDF.filter(
                 clientDF.country.isin(countries)
             ).select(
                 clientDF.id, clientDF.email, clientDF.country
             )
     else:
+        logging.info('Country list is empty, skipping country filter')
         clientDF = clientDF.select(
                 clientDF.id, clientDF.email, clientDF.country
             )
@@ -44,8 +59,10 @@ def process_data(clientDF: DataFrame,
             financialDF.id, financialDF.btc_a, financialDF.cc_t
         )
 
+    logging.info('Merging datasets...')
     df = clientDF.join(financialDF, ['id'])
     df = df.select([col(c).alias(rename.get(c, c)) for c in df.columns])
+    logging.info('Number of lines in final dataset: {}'.format(df.count()))
     return df
 
 
@@ -68,6 +85,11 @@ def main(client_csv: str,
     """
     main
     """
+    logging.basicConfig(format='%(asctime)s - %(levelname)s: %(message)s',
+                        filename='logs/pyspark-assignment.log',
+                        level=logging.INFO)
+    logging.critical('Starting')
+
     spark = create_session()
     clientDF, financialDF = read_data(spark, client_csv, financial_csv)
     df = process_data(clientDF, financialDF, countries, rename)
@@ -82,4 +104,4 @@ if __name__ == '__main__':
     parser.add_argument('--outdir', dest='outdir', default='client_data/')
     args = parser.parse_args()
 
-    main(args.client_csv, args.financial_csv, args.countries, outdir=args.outdir)
+    main(args.client_csv, args.financial_csv, args.countries)
